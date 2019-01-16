e5df1e75d13456722671291553f0dc13bcdb3598
RichardZhu123 <z-zrichard@hotmail.com>
2019-01-16 17:09:10 -0500

    First Commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://rich123:rich@ds125048.mlab.com:25048/mycryptohippo',   //prod
+  database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
